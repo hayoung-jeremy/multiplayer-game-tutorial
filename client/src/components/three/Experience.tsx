0d629783a5455d7bc3b831ef3194a2326f565ba1
Ha young, Kim <74491172+hayoung-jeremy@users.noreply.github.com>
2023-10-20 13:40:29 +0900

    feat : clone skinned mesh to use it multiple times

--- client/src/components/three/Experience.tsx
+++ client/src/components/three/Experience.tsx
@@ -1,14 +1,17 @@
-import { Environment, OrbitControls } from "@react-three/drei";
+import { ContactShadows, Environment, OrbitControls } from "@react-three/drei";
 import HoodieCharacter from "./HoodieCharacter";
 
 const Experience = () => {
   return (
     <>
-      <HoodieCharacter />
+      <HoodieCharacter hairColor="pink" topColor="blue" bottomColor="green" />
+      <HoodieCharacter position-x={1} hairColor="red" topColor="yellow" bottomColor="white" />
       <OrbitControls />
+      <ContactShadows blur={2} />
       <Environment preset="sunset" />
     </>
   );
 };
 
 export default Experience;
+ 
--- client/src/components/three/HoodieCharacter.tsx
+++ client/src/components/three/HoodieCharacter.tsx
@@ -1,7 +1,8 @@
-import { useRef } from "react";
+import { useEffect, useMemo, useRef, useState } from "react";
 import * as THREE from "three";
 import { useGLTF, useAnimations } from "@react-three/drei";
-import { GLTF } from "three-stdlib";
+import { GLTF, SkeletonUtils } from "three-stdlib";
+import { useGraph } from "@react-three/fiber";
 
 type GLTFResult = GLTF & {
   nodes: {
@@ -55,10 +56,37 @@ type ActionName =
   | "CharacterArmature|Walk"
   | "CharacterArmature|Wave";
 
-export default function HoodieCharacter(props: JSX.IntrinsicElements["group"]) {
+interface Props {
+  hairColor: string;
+  topColor: string;
+  bottomColor: string;
+  props?: JSX.IntrinsicElements["group"];
+}
+
+export default function HoodieCharacter({
+  hairColor = "pink",
+  topColor = "red",
+  bottomColor = "brown",
+  ...props
+}: Props) {
   const group = useRef<THREE.Group>(null);
-  const { nodes, materials, animations } = useGLTF("/models/HoodieCharacter.glb") as GLTFResult;
+  const { scene, materials, animations } = useGLTF("/models/HoodieCharacter.glb") as GLTFResult;
+
+  const clone = useMemo(() => SkeletonUtils.clone(scene), [scene]);
+  const { nodes } = useGraph(clone);
+
   const { actions } = useAnimations(animations, group);
+
+  const [currentAnim, setCurrentAnim] = useState<ActionName>("CharacterArmature|Idle");
+
+  useEffect(() => {
+    actions[currentAnim]?.reset().fadeIn(0.5).play();
+
+    return () => {
+      actions[currentAnim]?.fadeOut(0.5);
+    };
+  }, [actions, currentAnim]);
+
   return (
     <group ref={group} {...props} dispose={null}>
       <group name="Root_Scene">
@@ -69,69 +97,75 @@ export default function HoodieCharacter(props: JSX.IntrinsicElements["group"]) {
           <group name="Casual_Feet" rotation={[-Math.PI / 2, 0, 0]} scale={100}>
             <skinnedMesh
               name="Casual_Feet_1"
-              geometry={nodes.Casual_Feet_1.geometry}
+              geometry={(nodes.Casual_Feet_1 as THREE.SkinnedMesh).geometry}
               material={materials.White}
-              skeleton={nodes.Casual_Feet_1.skeleton}
+              skeleton={(nodes.Casual_Feet_1 as THREE.SkinnedMesh).skeleton}
             />
             <skinnedMesh
               name="Casual_Feet_2"
-              geometry={nodes.Casual_Feet_2.geometry}
+              geometry={(nodes.Casual_Feet_2 as THREE.SkinnedMesh).geometry}
               material={materials.Purple}
-              skeleton={nodes.Casual_Feet_2.skeleton}
+              skeleton={(nodes.Casual_Feet_2 as THREE.SkinnedMesh).skeleton}
             />
           </group>
           <group name="Casual_Legs" rotation={[-Math.PI / 2, 0, 0]} scale={100}>
             <skinnedMesh
               name="Casual_Legs_1"
-              geometry={nodes.Casual_Legs_1.geometry}
+              geometry={(nodes.Casual_Legs_1 as THREE.SkinnedMesh).geometry}
               material={materials.Skin}
-              skeleton={nodes.Casual_Legs_1.skeleton}
+              skeleton={(nodes.Casual_Legs_1 as THREE.SkinnedMesh).skeleton}
             />
             <skinnedMesh
               name="Casual_Legs_2"
-              geometry={nodes.Casual_Legs_2.geometry}
+              geometry={(nodes.Casual_Legs_2 as THREE.SkinnedMesh).geometry}
               material={materials.LightBlue}
-              skeleton={nodes.Casual_Legs_2.skeleton}
-            />
+              skeleton={(nodes.Casual_Legs_2 as THREE.SkinnedMesh).skeleton}
+            >
+              <meshStandardMaterial color={bottomColor} />
+            </skinnedMesh>
           </group>
           <group name="Casual_Head" rotation={[-Math.PI / 2, 0, 0]} scale={100}>
             <skinnedMesh
               name="Casual_Head_1"
-              geometry={nodes.Casual_Head_1.geometry}
+              geometry={(nodes.Casual_Head_1 as THREE.SkinnedMesh).geometry}
               material={materials.Skin}
-              skeleton={nodes.Casual_Head_1.skeleton}
+              skeleton={(nodes.Casual_Head_1 as THREE.SkinnedMesh).skeleton}
             />
             <skinnedMesh
               name="Casual_Head_2"
-              geometry={nodes.Casual_Head_2.geometry}
+              geometry={(nodes.Casual_Head_2 as THREE.SkinnedMesh).geometry}
               material={materials.Eyebrows}
-              skeleton={nodes.Casual_Head_2.skeleton}
+              skeleton={(nodes.Casual_Head_2 as THREE.SkinnedMesh).skeleton}
             />
             <skinnedMesh
               name="Casual_Head_3"
-              geometry={nodes.Casual_Head_3.geometry}
+              geometry={(nodes.Casual_Head_3 as THREE.SkinnedMesh).geometry}
               material={materials.Eye}
-              skeleton={nodes.Casual_Head_3.skeleton}
+              skeleton={(nodes.Casual_Head_3 as THREE.SkinnedMesh).skeleton}
             />
             <skinnedMesh
               name="Casual_Head_4"
-              geometry={nodes.Casual_Head_4.geometry}
+              geometry={(nodes.Casual_Head_4 as THREE.SkinnedMesh).geometry}
               material={materials.Hair}
-              skeleton={nodes.Casual_Head_4.skeleton}
-            />
+              skeleton={(nodes.Casual_Head_4 as THREE.SkinnedMesh).skeleton}
+            >
+              <meshStandardMaterial color={hairColor} />
+            </skinnedMesh>
           </group>
           <group name="Casual_Body" position={[0, 0.007, 0]} rotation={[-Math.PI / 2, 0, 0]} scale={100}>
             <skinnedMesh
               name="Casual_Body_1"
-              geometry={nodes.Casual_Body_1.geometry}
+              geometry={(nodes.Casual_Body_1 as THREE.SkinnedMesh).geometry}
               material={materials.Purple}
-              skeleton={nodes.Casual_Body_1.skeleton}
-            />
+              skeleton={(nodes.Casual_Body_1 as THREE.SkinnedMesh).skeleton}
+            >
+              <meshStandardMaterial color={topColor} />
+            </skinnedMesh>
             <skinnedMesh
               name="Casual_Body_2"
-              geometry={nodes.Casual_Body_2.geometry}
+              geometry={(nodes.Casual_Body_2 as THREE.SkinnedMesh).geometry}
               material={materials.Skin}
-              skeleton={nodes.Casual_Body_2.skeleton}
+              skeleton={(nodes.Casual_Body_2 as THREE.SkinnedMesh).skeleton}
             />
           </group>
         </group>
