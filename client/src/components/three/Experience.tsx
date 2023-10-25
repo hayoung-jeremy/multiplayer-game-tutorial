a4ce056e4b33ba5bf0c839bea0b4eb25c685ced3
Ha young, Kim <74491172+hayoung-jeremy@users.noreply.github.com>
2023-10-25 17:08:53 +0900

    feat : move items in build mode

--- client/src/components/three/Experience.tsx
+++ client/src/components/three/Experience.tsx
@@ -13,19 +13,20 @@ import { socket } from "@/socket";
 import { useGrid } from "@/hooks";
 
 const Experience = () => {
-  const [isBuildMode, setIsBuildMode] = useState(true);
-  const [draggedItem, setDraggedItem] = useState<any>(null);
-  const [dragPosition, setDragPosition] = useState<[number, number] | null>(null);
+  const scene = useThree(state => state.scene);
 
-  const [isOnFloor, setIsOnFloor] = useState(false);
-  useCursor(isOnFloor);
   const characters = useAtomValue(charactersAtom);
   const gameMap = useAtomValue(mapAtom);
   const user = useAtomValue(userAtom);
 
-  const scene = useThree(state => state.scene);
-
   const { vector3ToGrid, gridToVector3 } = useGrid();
+
+  const [isBuildMode, setIsBuildMode] = useState(true);
+  const [draggedItem, setDraggedItem] = useState<any>(null);
+  const [dragPosition, setDragPosition] = useState<[number, number] | null>(null);
+  const [items, setItems] = useState(gameMap?.gameItems);
+  const [isOnFloor, setIsOnFloor] = useState(false);
+  useCursor(isOnFloor);
 
   const onCharacterMove = (e: any) => {
     const player = scene.getObjectByName(`character-${user}`);
@@ -35,10 +36,22 @@ const Experience = () => {
     socket.emit("move", vector3ToGrid(player.position), vector3ToGrid(e.point));
   };
 
+  const onNavigateBuildMode = (e: any) => {
+    if (draggedItem !== null) {
+      setItems(prev => {
+        const newItems = [...prev];
+        newItems[draggedItem].gridPosition = vector3ToGrid(e.point);
+        return newItems;
+      });
+      setDraggedItem(null);
+    }
+  };
+
+  if (!gameMap) return;
+
   return (
     <>
-      {gameMap &&
-        !isBuildMode &&
+      {!isBuildMode &&
         characters.map(character => (
           <HoodieCharacter
             key={character.id}
@@ -51,40 +64,37 @@ const Experience = () => {
           />
         ))}
 
-      {gameMap &&
-        gameMap.gameItems.map((item, idx) => {
-          return (
-            <Item
-              key={`${item}-${idx}`}
-              item={item}
-              onClick={() => setDraggedItem((prev: null) => (prev === null ? idx : prev))}
-              isDragging={draggedItem === idx}
-              dragPosition={dragPosition}
-            />
-          );
-        })}
+      {(isBuildMode ? items : gameMap.gameItems)!.map((item, idx) => {
+        return (
+          <Item
+            key={`${item}-${idx}`}
+            item={item}
+            onClick={() => setDraggedItem((prev: null) => (prev === null ? idx : prev))}
+            isDragging={draggedItem === idx}
+            dragPosition={dragPosition}
+          />
+        );
+      })}
 
-      {gameMap && (
-        <mesh
-          rotation={[-Math.PI / 2, 0, 0]}
-          position-x={gameMap.size[0] / 2}
-          position-y={-0.001}
-          position-z={gameMap.size[1] / 2}
-          onClick={onCharacterMove}
-          onPointerEnter={() => setIsOnFloor(true)}
-          onPointerLeave={() => setIsOnFloor(false)}
-          onPointerMove={e => {
-            if (!isBuildMode) return;
-            const newPosition = vector3ToGrid(e.point);
-            if (!dragPosition || dragPosition[0] !== newPosition[0] || dragPosition[1] !== newPosition[1]) {
-              setDragPosition(newPosition);
-            }
-          }}
-        >
-          <planeGeometry args={gameMap.size} />
-          <meshStandardMaterial color="#f0f0f0" />
-        </mesh>
-      )}
+      <mesh
+        rotation={[-Math.PI / 2, 0, 0]}
+        position-x={gameMap.size[0] / 2}
+        position-y={-0.001}
+        position-z={gameMap.size[1] / 2}
+        onClick={isBuildMode ? onNavigateBuildMode : onCharacterMove}
+        onPointerEnter={() => setIsOnFloor(true)}
+        onPointerLeave={() => setIsOnFloor(false)}
+        onPointerMove={e => {
+          if (!isBuildMode) return;
+          const newPosition = vector3ToGrid(e.point);
+          if (!dragPosition || dragPosition[0] !== newPosition[0] || dragPosition[1] !== newPosition[1]) {
+            setDragPosition(newPosition);
+          }
+        }}
+      >
+        <planeGeometry args={gameMap.size} />
+        <meshStandardMaterial color="#f0f0f0" />
+      </mesh>
       <Grid infiniteGrid fadeDistance={50} fadeStrength={5} />
 
       <OrbitControls />
