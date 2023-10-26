cc761f71bcc27d531409559ca5e76fa57e6d40ab
Ha young, Kim <74491172+hayoung-jeremy@users.noreply.github.com>
2023-10-26 10:17:45 +0900

    chore : add icons

---
+++ client/src/components/icons/Close.tsx
@@ -0,0 +1,11 @@
+import { IconProps } from "@/types/icons";
+
+const Close = ({ width, height }: IconProps) => {
+  return (
+    <svg height={height} width={width} fill="currentColor" viewBox="0 -960 960 960" xmlns="http://www.w3.org/2000/svg">
+      <path d="M256-227.692 227.692-256l224-224-224-224L256-732.308l224 224 224-224L732.308-704l-224 224 224 224L704-227.692l-224-224-224 224Z" />
+    </svg>
+  );
+};
+
+export default Close;
---
+++ client/src/components/icons/index.ts
@@ -0,0 +1,5 @@
+export { default as Close } from "./Close";
+export { default as Rotate360 } from "./Rotate360";
+export { default as Store } from "./Store";
+export { default as Undo } from "./Undo";
+export { default as ViewIn3D } from "./ViewIn3D";
